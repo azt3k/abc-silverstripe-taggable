7b65c3a9dadc53a3b7268a8e3a371a9f02efb77e
Aaron Latham-Ilari <az@dev-io.us>
2015-12-11 14:28:47 +1300

    such tests

---
+++ tests/TaggableTest.php
@@ -0,0 +1,18 @@
+<?php
+class TaggableTest extends SapphireTest
+{
+
+    public function testSetup() {
+        // todo - write tests
+    }
+
+    /**
+     * @depends testSetup
+     */
+    public function testSMTPMailerSetConf() {
+
+        // phpunit is a bit broken so we manually call the dependent tests;
+        $this->testSetup();
+
+    }
+}
